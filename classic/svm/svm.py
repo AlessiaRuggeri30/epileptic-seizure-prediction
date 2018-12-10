34b345f7bb0326e68c4e972e953fd8000ed73806
Alessia Ruggeri <alessia.ruggeri.30@gmail.com>
2018-12-10 20:33:18 +0100

    Started prediction with classic models

--- classic/svm/svm.py
+++ classic/svm/svm.py
@@ -3,18 +3,36 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.metrics import brier_score_loss, accuracy_score
 
-''' Load dataset '''
-# path = "/home/phait/datasets/ieeg/TWH056_Day-504_Clip-0-1.npz"     # server
-path = "../../dataset/TWH056_Day-504_Clip-0-1.npz"                     # local
+n_clip = 3
+X = {}
+y = {}
+delimiters = {1: {'start': 1640000, 'end': 1740000},
+              2: {'start': 150000, 'end': 250000},
+              3: {'start': 60000, 'end': 160000}}
 
-with np.load(path) as data:
-    data = dict(data)
+for c in range(1, n_clip+1):
 
-data['szr_bool'] = data['szr_bool'].astype(int)     # one-hot encoding
+    ''' Load dataset '''
+    path = f"/home/phait/datasets/ieeg/TWH056_Day-504_Clip-0-{c}.npz"     # server
+    # path = "../../dataset/TWH056_Day-504_Clip-0-1.npz"                     # local
 
-X = data['ieeg'].T[1670000:]
-y = data['szr_bool'][1670000:]
-print(X.shape, y.shape)
+    with np.load(path) as data:
+        data = dict(data)
+
+    data['szr_bool'] = data['szr_bool'].astype(int)     # one-hot encoding
+
+    X[c] = data['ieeg'].T[delimiters[c]['start']:delimiters[c]['end']]
+    y[c] = data['szr_bool'][delimiters[c]['start']:delimiters[c]['end']]
+
+
+''' Select training set and test set '''
+X_training = np.concatenate((X[2], X[3]), axis=0)
+y_training = np.concatenate((y[2], y[3]), axis=0)
+X_test = X[1]
+y_test = y[1]
+
+print(X_training.shape, y_training.shape)
+print(X_test.shape, y_test.shape)
 
 print("Creating the svm classifier...")
 gamma = 'scale'
@@ -24,22 +42,20 @@ clf = svm.SVC(gamma=gamma)
 
 print("Fitting training data to the svm classifier...")
 if weighted is False:
-    clf.fit(X, y)
+    clf.fit(X_training, y_training)
 else:
     weighted = class_weight = {1: 10}
-    clf.fit(X, y, class_weight=class_weight)
+    clf.fit(X_training, y_training, class_weight=class_weight)
 
 print("Predicting values on test data...")
-predictions = clf.predict(X)
-errors = abs(predictions - y)
+predictions = clf.predict(X_test)
+errors = abs(predictions - y_test)
 
 print("Results")
-loss = round(brier_score_loss(y, predictions), 4)
-accuracy = round(accuracy_score(y, predictions), 4)
+loss = round(brier_score_loss(y_test, predictions), 4)
+accuracy = round(accuracy_score(y_test, predictions), 4)
 print(f"\tLoss:\t\t{loss}")
 print(f"\tAccuracy:\t{accuracy}")
-
-# print(errors[14300:30000])
 
 
 ''' Write results into file '''
@@ -48,7 +64,7 @@ if on_seizure:
 else:
     dataset = "on_whole_data"
 
-file_name = f"svm(gamma={gamma},weighted={weighted}," \
+file_name = f"svm(gamma={gamma},weighted={weighted})" \
     f"-{dataset}.txt"
 with open(file_name, 'w') as file:
     file.write("EXPERIMENT: SVM\n\n")
--- data_description/explore_data.py
+++ data_description/explore_data.py
@@ -23,8 +23,11 @@ for c in range(1, n_clip+1):
     with np.load(path) as data:
         data = dict(data)
 
-    ''' Check how many seizure there are ('s' is the resulting dictionary of seizures)'''
+    print(f"Clip {c} info:")
+    print(f"\tNumber of electrodes: {data['ieeg'].shape[0]}\n")
+    print(f"\tNumber of timestamps: {data['time_of_day_sec'].shape[0]}\n\n")
 
+    ''' Check how many seizure there are ('s' is the resulting dictionary of seizures)'''
     for i in range(1, data['time_of_day_sec'].shape[0]):
         if data['szr_bool'][i] == True and data['szr_bool'][i-1] == False:
             seizures[f"seizure{k}"] = {'start_idx': i-1}
@@ -47,31 +50,29 @@ print(seizures)
     This means that the seizure is not characterised by che intensity of the signal,
     but by the relations between the electrodes signal variations.'''
 
-
-# TODO correlation matrix (heatmap)
 # print(np.correlate(data['ieeg'][0:15000]))
 # print(np.correlate(data['ieeg'][:, s['seizure1']['start_idx']:s['seizure1']['end_idx']]))
 # print(np.correlate(data['ieeg'][:, 1692500:s['seizure1']['end_idx']]))
 
 
 ''' Write data description into file '''
-total = True
-# file_name = "data_description.txt"
-file_name = "data_description_total.txt"
-with open(file_name, 'w') as file:
-    file.write("DATA DESCRIPTION\n\n")
-
-    if not total:
-        file.write("Dataset keys:\n")
-        file.write(str(list(data.keys())) + "\n\n")
-
-        file.write(f"Number of electrodes: {data['ieeg'].shape[0]}\n")
-        file.write(f"Number of timestamps: {data['time_of_day_sec'].shape[0]}\n\n")
-
-        file.write(f"Max voltage:\t{np.max(data['ieeg'])}\n")
-        file.write(f"Min voltage:\t{np.min(data['ieeg'])}\n")
-        file.write(f"Voltage mean:\t{np.mean(data['ieeg'])}\n")
-        file.write(f"Voltage std:\t{np.std(data['ieeg'])}\n")
-
-    file.write(f"Seizures:\n")
-    file.write(str(seizures) + "\n\n")
+# total = True
+# # file_name = "data_description.txt"
+# file_name = "data_description_total.txt"
+# with open(file_name, 'w') as file:
+#     file.write("DATA DESCRIPTION\n\n")
+#
+#     if not total:
+#         file.write("Dataset keys:\n")
+#         file.write(str(list(data.keys())) + "\n\n")
+#
+#         file.write(f"Number of electrodes: {data['ieeg'].shape[0]}\n")
+#         file.write(f"Number of timestamps: {data['time_of_day_sec'].shape[0]}\n\n")
+#
+#         file.write(f"Max voltage:\t{np.max(data['ieeg'])}\n")
+#         file.write(f"Min voltage:\t{np.min(data['ieeg'])}\n")
+#         file.write(f"Voltage mean:\t{np.mean(data['ieeg'])}\n")
+#         file.write(f"Voltage std:\t{np.std(data['ieeg'])}\n")
+#
+#     file.write(f"Seizures:\n")
+#     file.write(str(seizures) + "\n\n")
