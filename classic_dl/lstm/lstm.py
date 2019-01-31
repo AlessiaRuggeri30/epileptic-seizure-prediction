c2f329e5dee8041d4b5203a4cf3872623d7d90d9
Alessia Ruggeri <alessia.ruggeri.30@gmail.com>
2019-01-31 13:00:23 +0100

    Changes to the lstm network

--- classic_dl/lstm/lstm.py
+++ classic_dl/lstm/lstm.py
@@ -16,7 +16,7 @@ seizure = {1: {'start': 1684381 - interval[1]['start'], 'end': 1699381 - interva
            2: {'start': 188013 - interval[2]['start'], 'end': 201013 - interval[2]['start']},
            3: {'start': 96699 - interval[3]['start'], 'end': 110699 - interval[3]['start']}}
 class_weight = {0: 1,
-                1: 5}
+                1: 3}
 
 """ Load datasets """
 for c in range(1, n_clip + 1):
@@ -64,6 +64,7 @@ epochs = 15
 batch_size = 64
 
 model = Sequential()
+model.add(LSTM(128, dropout=0.5, recurrent_dropout=0.5, return_sequences=True))
 model.add(LSTM(128, dropout=0.5, recurrent_dropout=0.5, return_sequences=True))
 model.add(LSTM(128, dropout=0.5, recurrent_dropout=0.5))
 model.add(Dropout(0.5))
