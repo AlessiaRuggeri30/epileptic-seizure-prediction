9dca12e3aaea865f074d68fa3c2d42843e19c8b4
Alessia Ruggeri <alessia.ruggeri.30@gmail.com>
2019-03-31 11:42:14 +0200

    Added conv model

---
+++ classic_dl/convolutional/conv_model.py
@@ -0,0 +1,27 @@
+from keras.layers import Dense, Dropout, Conv2D, BatchNormalization, Flatten, MaxPooling2D
+from keras.models import Sequential
+
+
+def build_conv_model(depth_conv, depth_dense, filters, kernel_size, reg, activation, batch_norm, dropout):
+    model = Sequential()
+    # conv + pooling layers
+    for i in range(depth_conv):
+        if i != 0:
+            if batch_norm:
+                model.add(BatchNormalization())
+            model.add(Dropout(dropout))
+        if i == (depth_conv-1):
+            model.add(Conv2D(filters, kernel_size=64, activation=activation, kernel_regularizer=reg))
+        else:
+            model.add(Conv2D(filters, kernel_size=kernel_size, activation=activation, kernel_regularizer=reg))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+    # dense layers
+    for k in range(depth_dense):
+        if batch_norm:
+            model.add(BatchNormalization())
+        model.add(Dropout(dropout))
+        if k == (depth_dense-1):
+            model.add(Dense(1, activation='sigmoid', kernel_regularizer=reg))
+        else:
+            model.add(Dense(256, activation=activation, kernel_regularizer=reg))
+    return model
