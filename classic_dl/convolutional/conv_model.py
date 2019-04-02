56e89727f02673e46901dc3c2eb7b530548c53cb
Alessia Ruggeri <alessia.ruggeri.30@gmail.com>
2019-04-02 09:48:41 +0200

    Removed regularization and dropout from convolutional layers

--- classic_dl/convolutional/conv_model.py
+++ classic_dl/convolutional/conv_model.py
@@ -6,10 +6,10 @@ def build_conv_model(depth_conv, depth_dense, filters, kernel_size, reg, activat
     model = Sequential()
     # conv + pooling layers
     for i in range(depth_conv):
-        if i != 0:
-            if batch_norm:
-                model.add(BatchNormalization())
-            model.add(Dropout(dropout))
+        # if i != 0:
+        #     if batch_norm:
+        #         model.add(BatchNormalization())
+        #     model.add(Dropout(dropout))
         if i == (depth_conv-1):
             if i == 0:
                 model.add(Conv1D(filters=128, kernel_size=kernel_size, activation=activation, kernel_regularizer=reg,
@@ -26,11 +26,11 @@ def build_conv_model(depth_conv, depth_dense, filters, kernel_size, reg, activat
     model.add(Flatten())
     # dense layers
     for k in range(depth_dense):
-        if batch_norm:
-            model.add(BatchNormalization())
-        model.add(Dropout(dropout))
         if k == (depth_dense-1):
             model.add(Dense(1, activation='sigmoid', kernel_regularizer=reg))
         else:
             model.add(Dense(256, activation=activation, kernel_regularizer=reg))
+            if batch_norm:
+                model.add(BatchNormalization())
+            model.add(Dropout(dropout))
     return model
