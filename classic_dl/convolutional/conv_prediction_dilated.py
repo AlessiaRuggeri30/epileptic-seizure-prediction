3a4bc7e7bcad052a303829093a2c3d48f91f5a16
Alessia Ruggeri <alessia.ruggeri.30@gmail.com>
2019-04-23 18:48:48 +0200

    Changed params

--- classic_dl/convolutional/conv_prediction_dilated.py
+++ classic_dl/convolutional/conv_prediction_dilated.py
@@ -38,7 +38,7 @@ X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
 """ Neural network hyperparameters """
-num = 17
+num = 21
 
 epochs = 10
 batch_size = 64
@@ -46,11 +46,11 @@ depth_conv = [2]     # search
 depth_dense = 2
 filters = [64]
 kernel_size = [3]    # search
-reg_n = ['5e-2', '5e-1']
+reg_n = ['5e-1']
 activation = 'relu'
 batch_norm = True
-dropout = [0.4, 0.5]
-pooling = True
+dropout = [0.4]
+pooling = False
 pool_size = 2
 padding = 'causal'
 dilation_rate = 3
