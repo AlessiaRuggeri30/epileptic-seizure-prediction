a48d695351a418a7673daf54d38d1b2a72550668
Alessia Ruggeri <alessia.ruggeri.30@gmail.com>
2019-07-11 18:24:17 +0200

    New experiment

--- graph-based_dl/lstm/graph_lstm_prediction.py
+++ graph-based_dl/lstm/graph_lstm_prediction.py
@@ -29,16 +29,16 @@ num = 21
 
 """ Neural network hyperparameters """
 epochs = [150]
-batch_size = 48
+batch_size = 64
 depth_lstm = [1]
-depth_dense = [2]
+depth_dense = [1]
 units_lstm = [256]
 g_filters = [32]
 reg_n = ['5e-3']
 activation = ['relu']
 batch_norm = False    # Keep it always False, since adding it leads to inconsistent results
 dropout = [0.4]
-learning_rate = [1e-4]
+learning_rate = [1e-3]
 
 """ Functional connectivity hyperparameters """
 band_freq = (70., 100.)
