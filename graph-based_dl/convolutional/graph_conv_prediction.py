64057d53ded8251b9ae13703375d6a6f698c3dc7
Alessia Ruggeri <alessia.ruggeri.30@gmail.com>
2019-07-25 13:06:36 +0200

    New experiment

--- graph-based_dl/convolutional/graph_conv_prediction.py
+++ graph-based_dl/convolutional/graph_conv_prediction.py
@@ -48,7 +48,7 @@ learning_rate = [1e-3]
 """ Functional connectivity hyperparameters """
 band_freq = (70., 100.)
 sampling_freq = 500.
-samples_per_graph = [100]
+samples_per_graph = [200]
 # fc_measure = 'corr'
 link_cutoff = 0.
 percentiles = (40, 60)
@@ -61,7 +61,7 @@ percentiles = (40, 60)
 """ Sequences hyperparameters """
 subsampling_factor = [1]
 stride = [2]
-look_back = [1000]
+look_back = [2000]
 target_steps_ahead = [2000]  # starting from the position len(sequence)
 predicted_timestamps = 1
 
