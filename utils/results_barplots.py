e8335db599be0037169562eb15153d2ed9eec0ad
Alessia Ruggeri <alessia.ruggeri.30@gmail.com>
2019-08-25 10:56:37 +0200

    Generating results barplots

--- utils/results_barplots.py
+++ utils/results_barplots.py
@@ -5,23 +5,25 @@ import matplotlib.pyplot as plt
 N = 4       # num of metrics (loss, acc, roc-auc, recall)
 ind = np.arange(N)      # the x locations for the metrics
 width = 0.2        # the width of the bars
-saving_name = "ml_models"
+
+''' File name and path '''
+saving_name = "pred_sequence2000_plots"
 saving_path = f"./results_plots/{saving_name}.png"
 
 ''' Metrics parameters '''
 # The length of each list should be always N, while the number of lists corresponds to the number of models
-model1_vals = [0.3, 0.9, 0.77, 0.12]
-model2_vals = [0.3, 0.9, 0.8, 0.22]
-model3_vals = [2.5, 0.75, 0.6, 0.07]
-model4_vals = [0.29, 0.9, 0.73, 0.25]
-y_max = 2.75      # 3
-y_step = 0.25      # 0.25
+model1_vals = [0.236, 0.940, 0.911, 0.604]
+model2_vals = [0.204, 0.956, 0.932, 0.767]
+model3_vals = [2.549, 0.328, 0.642, 0.790]
+model4_vals = [2.103, 0.467, 0.475, 0.463]
+y_max = 2.75
+y_step = 0.25
 
 ''' Models names '''
-model1 = 'random forest'
-model2 = 'gradient boosti ng'
-model3 = 'SVM'
-model4 = 'FCNN'
+model1 = 'CNN'
+model2 = 'LSTM'
+model3 = 'graph-based CNN'
+model4 = 'graph-based LSTM'
 
 ''' Figure generation '''
 fig = plt.figure()
@@ -41,17 +43,97 @@ ax.set_xticks(ind+(1/N))
 ax.set_xticklabels(('Loss', 'Accuracy', 'ROC-AUC', 'Recall'))
 plt.yticks(np.arange(0, y_max+0.1, y_step))
 ax.legend(rects_labels, names)
-
-# def autolabel(rects, vals):
-#     for i in range(len(rects)):
-#         h = vals[i]
-#         ax.text(rects[i].get_x()+rects[i].get_width()/2., 1.05*h, '%s'%h,
-#                 ha='center', va='bottom')
-#
-# autolabel(rects1, model1_vals)
-# autolabel(rects2, model2_vals)
-# autolabel(rects3, model3_vals)
-# autolabel(rects4, model4_vals)
 plt.tight_layout()
 
 plt.savefig(saving_path)
+
+# -----------------------------------------------------------------------------
+# MODELS PARAMETERS
+# -----------------------------------------------------------------------------
+
+# DETECTION ON A TIME STEP
+# ''' File name and path '''
+# saving_name = "det_timestep_plots"
+# saving_path = f"./results_plots/{saving_name}.png"
+#
+# ''' Metrics parameters '''
+# # The length of each list should be always N, while the number of lists corresponds to the number of models
+# model1_vals = [2.497, 0.751, 0.601, 0.067]
+# model2_vals = [0.303, 0.899, 0.768, 0.121]
+# model3_vals = [0.297, 0.898, 0.805, 0.219]
+# model4_vals = [0.293, 0.902, 0.734, 0.248]
+# y_max = 2.75
+# y_step = 0.25
+#
+# ''' Models names '''
+# model1 = 'SVM'
+# model2 = 'random forest'
+# model3 = 'gradient boosting'
+# model4 = 'FCNN'
+
+
+# DETECTION ON A SEQUENCE
+# ''' File name and path '''
+# saving_name = "det_sequence_plots"
+# saving_path = f"./results_plots/{saving_name}.png"
+#
+# ''' Metrics parameters '''
+# # The length of each list should be always N, while the number of lists corresponds to the number of models
+# model1_vals = [0.208, 0.942, 0.903, 0.534]
+# model2_vals = [0.221, 0.953, 0.910, 0.580]
+# model3_vals = [2.268, 0.297, 0.667, 0.895]
+# model4_vals = [1.484, 0.692, 0.616, 0.476]
+# y_max = 2.5
+# y_step = 0.25
+#
+# ''' Models names '''
+# model1 = 'CNN'
+# model2 = 'LSTM'
+# model3 = 'graph-based CNN'
+# model4 = 'graph-based LSTM'
+
+
+# PREDICTION ON A SEQUENCE 500
+# ''' File name and path '''
+# saving_name = "pred_sequence500_plots"
+# saving_path = f"./results_plots/{saving_name}.png"
+#
+# ''' Metrics parameters '''
+# # The length of each list should be always N, while the number of lists corresponds to the number of models
+# model1_vals = [0.340, 0.892, 0.946, 0.634]
+# model2_vals = [0.293, 0.922, 0.922, 0.708]
+# model3_vals = [1.812, 0.291, 0.570, 0.877]
+# model4_vals = [1.532, 0.674, 0.558, 0.377]
+# y_max = 2
+# y_step = 0.25
+
+
+# PREDICTION ON A SEQUENCE 1000
+# ''' File name and path '''
+# saving_name = "pred_sequence1000_plots"
+# saving_path = f"./results_plots/{saving_name}.png"
+#
+# ''' Metrics parameters '''
+# # The length of each list should be always N, while the number of lists corresponds to the number of models
+# model1_vals = [0.224, 0.924, 0.930, 0.702]
+# model2_vals = [0.180, 0.954, 0.908, 0.637]
+# model3_vals = [2.990, 0.329, 0.661, 0.779]
+# model4_vals = [1.524, 0.614, 0.547, 0.439]
+# y_max = 3
+# y_step = 0.25
+
+
+# PREDICTION ON A SEQUENCE 2000
+''' File name and path '''
+# saving_name = "pred_sequence2000_plots"
+# saving_path = f"./results_plots/{saving_name}.png"
+#
+# ''' Metrics parameters '''
+# # The length of each list should be always N, while the number of lists corresponds to the number of models
+# model1_vals = [0.236, 0.940, 0.911, 0.604]
+# model2_vals = [0.204, 0.956, 0.932, 0.767]
+# model3_vals = [2.549, 0.328, 0.642, 0.790]
+# model4_vals = [2.103, 0.467, 0.475, 0.463]
+# y_max = 2.75
+# y_step = 0.25
+
